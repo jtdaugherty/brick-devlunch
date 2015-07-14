f2fad46e4b27cfcb585f3f23fe0952ea87e531ae
Jonathan Daugherty <jtd@galois.com>
2015-07-14 11:54:19 -0700

    Main: add some imports we will need

--- programs/Main.hs
+++ programs/Main.hs
@@ -1,11 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import Control.Applicative ((<$>))
 import Control.Monad (void)
+import Control.Lens
 import Data.Default
+import Data.Monoid
 import Graphics.Vty
 
 import Model
+
+import Brick.Main
+import Brick.Util
+import Brick.AttrMap
+import Brick.Widgets.Core
+import Brick.Widgets.Border
 
 main :: IO ()
 main = return ()
