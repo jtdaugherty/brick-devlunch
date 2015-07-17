8f12115b10f7c3443f07700d5bc98193d6008555
Jonathan Daugherty <jtd@galois.com>
2015-07-17 13:19:32 -0700

    Update main

--- programs/Main.hs
+++ programs/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Control.Applicative ((<$>))
 import Control.Monad (void)
 import Control.Lens
+import Data.List (intersperse)
 import Data.Default
 import Data.Monoid
 import Graphics.Vty
@@ -15,7 +16,90 @@ import Brick.Main
 import Brick.Util
 import Brick.AttrMap
 import Brick.Widgets.Core
+import Brick.Widgets.Center
 import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+
+data St =
+    St { _gameState :: (Player, Board)
+       , _cursor :: (Int, Int)
+       }
+
+makeLenses ''St
+
+initialState :: St
+initialState = St (X, newBoard) (0, 0)
+
+cursorAttr :: AttrName
+cursorAttr = "cursor"
+
+boardAttr :: AttrName
+boardAttr = "board"
+
+drawUI :: St -> [Widget]
+drawUI (St (p, b) cur) = [ui]
+    where
+        ui = center $ withBorderStyle unicode $
+             currentPlayer p <=>
+             drawBoard cur b <=>
+             drawGameStatus b
+        currentPlayer pl = str $ "Current player: " <> (show pl)
+        drawGameStatus b =
+            case gameStatus b of
+                Won p -> str $ show p <> " won!"
+                InProgress -> str "Make a move."
+                NoMovesLeft -> str "No moves left, game over!"
+
+drawBoard :: (Int, Int) -> Board -> Widget
+drawBoard cursor board = border $ withDefaultAttr boardAttr rows
+    where
+        rows = vBox $ intersperse rowBorder $
+                      drawRow <$> zip [0..] (toList board)
+        rowBorder = hBox $ intersperse (borderElem bsIntersectFull) $
+                           replicate 3 $ hLimit 5 hBorder
+        drawRow (r, row) = hBox $
+          intersperse (vLimit 3 vBorder) $
+            [ drawPiece ((r, c) == cursor) piece
+            | (c, piece) <- zip [0..] row
+            ]
+
+drawPiece :: Bool -> Maybe Player -> Widget
+drawPiece selected piece =
+    let w = str $ maybe " " show piece
+        attr = if selected then cursorAttr else boardAttr
+    in withAttr attr $
+       padLeftRight (Pad 2) $
+       padTopBottom (Pad 1) w
+
+appEvent :: St -> Event -> EventM (Next St)
+appEvent st (EvKey k []) =
+    case k of
+        KChar 'r'  -> continue initialState
+        KChar 'q'  -> halt st
+        KEsc       -> halt st
+        KChar ' '  -> continue $ st & gameState %~ move (st^.cursor)
+        KUp        -> continue $ st & cursor._1 %~ (max 0 . subtract 1)
+        KLeft      -> continue $ st & cursor._2 %~ (max 0 . subtract 1)
+        KDown      -> continue $ st & cursor._1 %~ (min 2 . (+ 1))
+        KRight     -> continue $ st & cursor._2 %~ (min 2 . (+ 1))
+        _          -> continue st
+appEvent st _ = continue st
+
+theMap :: AttrMap
+theMap = attrMap defAttr
+    [ (cursorAttr,  black `on` yellow)
+    , (boardAttr,   white `on` blue)
+    ]
+
+app :: App St Event
+app =
+    App { appDraw = drawUI
+        , appHandleEvent = appEvent
+        , appAttrMap = const theMap
+        , appChooseCursor = neverShowCursor
+        , appStartEvent = return
+        , appLiftVtyEvent = id
+        }
 
 main :: IO ()
-main = return ()
+main = void $ defaultMain app initialState
